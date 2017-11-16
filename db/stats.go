5f050d08326306f1b1a5fad627d51f97e1359f08
Trent Deal <trent.deal@gmail.com>
2017-11-15 21:00:16 -0500

    Add model for stats

---
+++ db/stats.go
@@ -0,0 +1,24 @@
+package db
+
+import "log"
+
+type Stat struct {
+	ID      int    `db:"id"`
+	Name    string `db:"name"`
+	AltName string `db:"alt_name"`
+}
+
+func (db DB) FindStatsByID(id int) (*Stat, error) {
+	var s Stat
+
+	err := db.QueryRowx(`
+	select id, name, alt_name from stats where id = $1
+	`, id).StructScan(&s)
+
+	if err != nil {
+		log.Println("Unable to execute query!")
+		return nil, err
+	}
+
+	return &s
+}
