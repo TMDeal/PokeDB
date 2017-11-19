36cc35e48498703bc59f3f05a5c5c128d0e66262
Trent Deal <trent.deal@gmail.com>
2017-11-19 15:40:34 -0500

    Root resolvers return a list instead of a single item

--- resolvers/root.go
+++ resolvers/root.go
@@ -1,6 +1,7 @@
 package resolvers
 
 import (
+	"database/sql"
 	"log"
 
 	"github.com/TMDeal/PokeDB/models"
@@ -19,20 +20,67 @@ func NewRootResolver(db *models.DB) *RootResolver {
 	}
 }
 
+type IDArgs struct {
+	ID *int32
+}
+
+type IDNameArgs struct {
+	ID   *int32
+	Name *string
+}
+
 //Generation resolves a Generation based on an ID
-func (root *RootResolver) Generations(args struct{ ID int32 }) *GenerationResolver {
-	gen, err := root.DB.FindGenerationByID(int(args.ID))
+func (root *RootResolver) Generations(args IDArgs) *[]*GenerationResolver {
+	var gr []*GenerationResolver
+
+	gens, err := root.DB.FindGenerations(int(*args.ID))
+	if err == sql.ErrNoRows {
+		return nil
+	}
 	if err != nil {
 		log.Fatal(err)
 	}
-	return NewGenerationResolver(gen)
+
+	for _, gen := range gens {
+		gr = append(gr, NewGenerationResolver(gen))
+	}
+
+	return &gr
 }
 
 //Region resolves a Region based on an ID
-func (root *RootResolver) Regions(args struct{ ID int32 }) *RegionResolver {
-	r, err := root.DB.FindRegionByID(int(args.ID))
+func (root *RootResolver) Regions(args IDNameArgs) *[]*RegionResolver {
+	var rr []*RegionResolver
+
+	rs, err := root.DB.FindRegions(int(*args.ID))
+	if err == sql.ErrNoRows {
+		return nil
+	}
 	if err != nil {
 		log.Fatal(err)
 	}
-	return NewRegionResolver(r)
+
+	for _, r := range rs {
+		rr = append(rr, NewRegionResolver(r))
+	}
+
+	return &rr
+}
+
+//Types resolves a Type based on an ID
+func (root *RootResolver) Types(args IDNameArgs) *[]*TypeResolver {
+	var tr []*TypeResolver
+
+	ts, err := root.DB.FindTypes(int(*args.ID))
+	if err == sql.ErrNoRows {
+		return nil
+	}
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, t := range ts {
+		tr = append(tr, NewTypeResolver(t))
+	}
+	return &tr
 }
