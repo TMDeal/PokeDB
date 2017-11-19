033b50f68ac8f5c105df7af0126f1a0532a506d0
Trent Deal <trent.deal@gmail.com>
2017-11-19 15:40:04 -0500

    Add resolver for damage classes

---
+++ resolvers/damage_class.go
@@ -0,0 +1,38 @@
+package resolvers
+
+import (
+	"strconv"
+
+	"github.com/TMDeal/PokeDB/models"
+	graphql "github.com/neelance/graphql-go"
+)
+
+//DamageClassResolver resolves a DamageClass for graphql
+type DamageClassResolver struct {
+	dc *models.DamageClass
+}
+
+func NewDamageClassResolver(dc *models.DamageClass) *DamageClassResolver {
+	return &DamageClassResolver{dc}
+}
+
+//ID resolves a damage classes ID
+func (dcr *DamageClassResolver) ID() graphql.ID {
+	id := graphql.ID(strconv.Itoa(dcr.dc.ID))
+	return id
+}
+
+//Identifier resolves a damage classes identifier
+func (dcr *DamageClassResolver) Identifier() string {
+	return dcr.dc.Identifier
+}
+
+//Name resolves a damage classes name
+func (dcr *DamageClassResolver) Name() string {
+	return dcr.dc.Name
+}
+
+//Description resolves a damage classes description
+func (dcr *DamageClassResolver) Description() string {
+	return dcr.dc.Description
+}
