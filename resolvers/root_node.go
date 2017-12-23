5638dbe954ca61bc0368513da736520c16fe8394
Trent Deal <trent.deal@gmail.com>
2017-12-23 12:39:39 -0500

    Move resolution of query type to new function

--- resolvers/root_node.go
+++ resolvers/root_node.go
@@ -5,55 +5,95 @@ import (
 	"github.com/TMDeal/PokeDB/models"
 )
 
-func (root *RootResolver) Node(args arguments.ID) *NodeResolver {
+func (root *RootResolver) Node(args arguments.ID) (*NodeResolver, error) {
 	if args.ID != nil {
-		var node Node
-
 		typ, id, err := FromGlobalID(*args.ID)
 		if err != nil {
-			return nil
+			return nil, err
 		}
 
-		switch typ {
-		case "Region":
-			var r models.Region
-			query := models.Select("*").From("regions").Where("id = ?", int(id))
-			if err := root.db.Find(&r, query); err != nil {
-				return nil
-			}
-
-			node = NewRegionResolver(root.db, &r)
-
-		case "Generation":
-			var gen models.Generation
-			query := models.Select("*").From("generations").Where("id = ?", int(id))
-			if err := root.db.Find(&gen, query); err != nil {
-				return nil
-			}
-
-			node = NewGenerationResolver(root.db, &gen)
-
-		case "Type":
-			var t models.Type
-			query := models.Select("*").From("types").Where("id = ?", int(id))
-			if err := root.db.Find(&t, query); err != nil {
-				return nil
-			}
-
-			node = NewTypeResolver(root.db, &t)
-
-		case "Move":
-			var m models.Move
-			query := models.Select("*").From("moves").Where("id = ?", int(id))
-			if err := root.db.Find(&m, query); err != nil {
-				return nil
-			}
-
-			node = NewMoveResolver(root.db, &m)
-		}
-
-		return NewNodeResolver(node)
+		return ResolveType(root.db, typ, int(id))
 	}
 
-	return nil
+	return nil, nil
+}
+
+func ResolveType(db *models.DB, t string, id int) (*NodeResolver, error) {
+	var node Node
+
+	switch t {
+	case "Region":
+		var r models.Region
+		query := models.Regions().Where("id = ?", id)
+		if err := db.Find(&r, query); err != nil {
+			return nil, err
+		}
+
+		node = NewRegionResolver(db, &r)
+
+	case "Move":
+		var m models.Move
+		query := models.Moves().Where("id = ?", id)
+		if err := db.Find(&m, query); err != nil {
+			return nil, err
+		}
+
+		node = NewMoveResolver(db, &m)
+
+	case "Generation":
+		var gen models.Generation
+		query := models.Generations().Where("id = ?", id)
+		if err := db.Find(&gen, query); err != nil {
+			return nil, err
+		}
+
+		node = NewGenerationResolver(db, &gen)
+
+	case "Stat":
+		var s models.Stat
+		query := models.Stats().Where("id = ?", id)
+		if err := db.Find(&s, query); err != nil {
+			return nil, err
+		}
+
+		node = NewStatResolver(db, &s)
+
+	case "Type":
+		var t models.Type
+		query := models.Types().Where("id = ?", id)
+		if err := db.Find(&t, query); err != nil {
+			return nil, err
+		}
+
+		node = NewTypeResolver(db, &t)
+
+	case "Version":
+		var v models.Version
+		query := models.Versions().Where("id = ?", id)
+		if err := db.Find(&v, query); err != nil {
+			return nil, err
+		}
+
+		node = NewVersionResolver(db, &v)
+
+	case "Ability":
+		var a models.Ability
+		query := models.Abilities().Where("id = ?", id)
+		if err := db.Find(&a, query); err != nil {
+			return nil, err
+		}
+
+		node = NewAbilityResolver(db, &a)
+
+	case "VersionGroup":
+		var vg models.VersionGroup
+		query := models.VersionGroups().Where("id = ?", id)
+		if err := db.Find(&vg, query); err != nil {
+			return nil, err
+		}
+
+		node = NewVersionGroupResolver(db, &vg)
+	}
+
+	return NewNodeResolver(node), nil
 }
